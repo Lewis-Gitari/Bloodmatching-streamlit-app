fa90d81c71e6f715b59ccea4a160315b8e967c0d
Lewis Gitari <lewisgitarimbogo@gmail.com>
2024-06-17 10:15:39 +0300

    Converteed the donor_df to a pandas df and changed the pd.append issue

--- Bloodmatching.py
+++ Bloodmatching.py
@@ -7,7 +7,7 @@ csv_file = 'donors.csv'
 if not os.path.isfile(csv_file):
     donors_df = pd.DataFrame(columns=[
         'id', 'name', 'blood_group', 'rh_factor', 
-         'phone_number', 'unit','Deployment','last_donation_date'
+         'phone_number', 'Pints', 'Unit','Deployment','last_donation_date'
     ])
     donors_df.to_csv(csv_file, index=False)
 else:
@@ -37,26 +37,30 @@ with st.form('add_donor_form'):
     rh_factor = st.selectbox('Rh Factor', ['+', '-'])
     location = st.text_input('Location')
     phone_number = st.text_input('Phone Number')
-    unit = st.number_input('Unit', min_value=0, step=1)
+    pints = st.number_input('Unit', min_value=0, step=1)
+    unit = st.text_input('unit')
     last_donation_date = st.date_input('Last Donation Date')
 
     submit_button = st.form_submit_button('Add Donor')
 
 if submit_button:
     # Create a new donor entry
-    new_donor = {
+    new_donor = pd.DataFrame ([{
         'id': id,
         'name': name,
         'blood_group': blood_group,
         'rh_factor': rh_factor,
         'location': location,
         'phone_number': phone_number,
+        'pints': pints,
         'unit': unit,
         'last_donation_date': last_donation_date
-    }
+    }])
     
     # Append new donor to the DataFrame
-    donors_df = donors_df.append(new_donor, ignore_index=True)
+    #donors_df = donors_df.append(new_donor, ignore_index=True)
+    donors_df = pd.concat([donors_df, new_donor], ignore_index=True)
+    
     
     # Save updated DataFrame to CSV
     donors_df.to_csv(csv_file, index=False)
